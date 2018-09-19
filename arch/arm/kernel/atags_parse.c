27251b3c0eaff4ba352d0c032e34de1da54588de
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-09-19 11:42:29 +0400

    fixed issue on bad machine atags reading: boot stops on kernel unpack
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- arch/arm/kernel/atags_parse.c
+++ arch/arm/kernel/atags_parse.c
@@ -192,7 +192,11 @@ setup_machine_tags(phys_addr_t __atags_pointer, unsigned int machine_nr)
 	 * locate machine in the list of supported machines.
 	 */
 	for_each_machine_desc(p)
+#ifdef CONFIG_ARCH_NUC970
+		if(1) {	// We only enable one machine type in kernel, so the first one must be what we're looking for.  --ya
+#else
 		if (machine_nr == p->nr) {
+#endif		
 			printk("Machine: %s\n", p->name);
 			mdesc = p;
 			break;
